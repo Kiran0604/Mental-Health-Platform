c39bb65a87ce5fea4f62d37aa8dda9d700570d78
Kiran R Aithal <126346131+Kiran0604@users.noreply.github.com>
2025-02-03 22:11:49 +0530

    Update app.py

--- DBMS LAB project Final/app.py
+++ DBMS LAB project Final/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import mysql.connector # type: ignore
 from datetime import date, datetime
 import regex as re
-
+import os
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with a secure key
 
