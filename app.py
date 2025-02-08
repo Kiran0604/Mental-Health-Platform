308684ad1ac9b009464ec8bb2539704376e66941
Kiran R Aithal <126346131+Kiran0604@users.noreply.github.com>
2025-02-08 14:26:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1623,4 +1623,5 @@ def chatbot():
     return render_template('dialogflow.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
